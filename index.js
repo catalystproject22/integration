8a970abc026e8052b7fe91bc1d34b585fa266b2e
Maor Roizman Gheiler <maor.roizman@utec.edu.pe>
2022-08-01 01:19:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-function funcion_externa(){
-  console.log("funcion_externa");
+var BASE_URL = 'https://integration-catalyst.herokuapp.com';
+var AF_URL = window.location.host;
+
+
+function setCookie(cookie_key, cookie_value, days_of_expiration) {
+  var date = new Date();
+  date.setTime(date.getTime() + (days_of_expiration*24*60*60*1000));
+  var expires = "expires="+ date.toUTCString();
+  document.cookie = cookie_key + "=" + cookie_value + ";" + expires + ";path=/";
 }
 
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function afCallAPI(data) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {};
+  xhttp.open("POST", BASE_URL, true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  
+  data['current_page_url'] = btoa(window.location.href);
+  data['base_url']    = btoa(AF_URL);
+  data['af_id']       = getCookie("af_id");
+  data['script_name'] = af_script;
+
+  var p = Object.keys(data).map(key => key + '=' + data[key]).join('&');
+  xhttp.send(p);
+}
 
 var AffTracker = {
-  print_data: function(data){
-    console.log(data);
-  },
-  test: function(){
-    console.log("Hola mundo");
-  },
-  call_funcion_externa: function(){
-    funcion_externa();
-  }
+  
 }
